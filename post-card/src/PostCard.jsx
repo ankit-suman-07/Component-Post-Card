9fcc0e6efaa04770aece206016c0502f77e820f9
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-08 02:38:38 +0530

    Post Card skeleton

--- post-card/src/PostCard.jsx
+++ post-card/src/PostCard.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const PostCard = () => {
   return (
-    <div>PostCard</div>
+    <div>
+        <div>
+            Post Header
+        </div>
+        <div>
+            Post Body
+        </div>
+        <div>
+            Post Footer
+        </div>
+        <div>
+            Comments
+        </div>
+    </div>
   )
 }
