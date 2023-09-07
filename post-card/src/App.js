e38d4ce14493f74b6977b23024b0aff43789c8a7
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-08 02:33:11 +0530

    Initial Commit

---
+++ post-card/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { PostCard } from './PostCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PostCard />
+    </div>
+  );
+}
+
+export default App;
